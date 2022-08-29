29396f5f4026d74635a26554a73598f6a895e545
xxx <xxx>
2022-08-29 21:59:32 +0800

    no message
    Match-id-bc7afd7b342dd1c7ba70c540ab378d460f4b8306

---
+++ framework/tarunner/src/ta_mt.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2019-2020. All rights reserved.
+ * Description: TA work thread function.
+ * Create: 2019-06-03
+ */
+#ifndef TA_MT_TA_MT_H
+#define TA_MT_TA_MT_H
+
+#include <stdint.h>
+#include <ta_routine.h> /* for ta_routine_info */
+
+#define INIT_BUILD 0
+#define NON_INIT_BUILD 1
+
+#define SECOND_CHANNEL 1
+#define DEFAULT_MSG_HANDLE 0
+#define CREATE_THREAD_FAIL 0xFFFFFFFFU /* same to gtask */
+void tee_task_entry_mt(ta_entry_type ta_entry, int32_t priority, const char *name,
+    const struct ta_routine_info *append_args);
+
+#endif
