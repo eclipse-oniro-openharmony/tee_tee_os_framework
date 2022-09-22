2a744d2399321c2c27ff3055804b20ed6257e2b2
xxx <xxx>
2022-09-22 10:02:57 +0800

    no message
    Match-id-07d3adda7d56cf46488d48a31d3666b2b5bcdf5a

--- trustedcore_hm/platform/ipc/demo_arm32/common/product_config.mk
+++
@@ -1,6 +0,0 @@
-#tee drivers core service
-
-export CONFIG_CRYPTO_SUPPORT_EC25519 := true
-export CONFIG_CRYPTO_SUPPORT_X509 := true
-export CONFIG_CRYPTO_ECC_WRAPPER := true
-export CONFIG_CRYPTO_AES_WRAPPER := true
