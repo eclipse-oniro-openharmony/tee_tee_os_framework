68fc7da01d02acded27a4a648cbc26e8d28c6317
xxx <xxx>
2022-10-12 10:33:33 +0800

    no message
    Match-id-405398a01efa78e517d6509fe015d804fa200887

---
+++ lib/syslib/libelf_verify/src/tee_perm_img.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) Huawei Technologies Co., Ltd. 2022-2022. All rights reserved.
+ * Description: tee app image load service head file
+ * Create: 2022.04.15
+ */
+#include "tee_perm_img.h"
+#include "handle_config.h"
+#include "tee_log.h"
+#include "securec.h"
+#include "ta_lib_img_unpack.h"
+#include "ta_verify_key.h"
+#include "tee_elf_verify.h"
+#include "tee_elf_verify_inner.h"
+#include "permission_service.h"
+
+TEE_Result get_config_cert_param(cert_param_t *cert_param, struct sign_config_t *config)
+{
+    if (cert_param == NULL || config == NULL)
+        return TEE_ERROR_BAD_PARAMETERS;
+
+    ta_payload_layer_t *ta_payload = get_ta_payload();
+    if (ta_payload->payload_hdr.ta_conf_size == 0)
+        return TEE_SUCCESS;
+
+    load_img_info *img_info = get_img_info();
+    if (img_info == NULL)
+        return TEE_ERROR_BAD_PARAMETERS;
+
+    cert_param->sys_verify_ta = img_info->manifest.ext.sys_verify_ta;
+    TEE_Result ret = tee_ext_set_config(ta_payload->ta_conf, ta_payload->payload_hdr.ta_conf_size,
+        &img_info->manifest.srv_uuid, (uint8_t *)img_info->manifest.service_name,
+        img_info->manifest.mani_info.service_name_len, cert_param);
+    if (ret != TEE_SUCCESS) {
+        tloge("Failed to set configuration\n");
+        return ret;
+    }
+    config->is_oh = (cert_param->cert_product_type == OH_CA_TYPE) ? true : false;
+    ta_payload->conf_registed = true;
+    return TEE_SUCCESS;
+}
+
+TEE_Result oh_get_verify_key(void **key, const struct sign_config_t *config, cert_param_t *cert_param)
+{
+    TEE_Result ret;
+    if (config == NULL || cert_param == NULL || key == NULL)
+        return TEE_ERROR_BAD_PARAMETERS;
+
+    uint32_t alg = config->sign_ta_alg;
+    uint32_t ca_type = get_ca_type();
+    if (ca_type == CA_PUBLIC) {
+        *key = &(cert_param->public_key);
+        ret = TEE_SUCCESS;
+    } else if (ca_type == CA_PRIVATE) {
+        if (cert_param->cert_type == TA_DEBUG_CERT) {
+            *key = &(cert_param->public_key);
+            ret = TEE_SUCCESS;
+        } else {
+            ret = oh_get_ta_pub_key(key, alg);
+        }
+    } else {
+        tloge("the ca type is invalid\n");
+        ret = TEE_ERROR_GENERIC;
+    }
+    return ret;
+}
