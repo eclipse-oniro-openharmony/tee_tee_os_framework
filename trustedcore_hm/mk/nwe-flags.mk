382ec25447efa74d56e68b4a76e23f00778fec58
xxx <xxx>
2022-10-20 17:42:18 +0800

    no message
    Match-id-0c56a23c6d1d819fa75e8c38bd73a54a2549d1ad

--- trustedcore_hm/mk/nwe-flags.mk
+++
@@ -1,8 +0,0 @@
-## initheaders
-inc-flags += -I$(PREBUILD_HEADER)/libinitheaders/include
-
-ifeq ($(ARCH),arm)
-inc-flags += -I$(PREBUILD_HEADER)/libinitheaders/include/arch/arm32
-else
-inc-flags += -I$(PREBUILD_HEADER)/libinitheaders/include/arch/aarch64
-endif
