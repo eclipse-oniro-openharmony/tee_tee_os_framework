842da381bb641623be555456266fc8cceff09669
xxx <xxx>
2022-10-24 21:07:11 +0800

    no message
    Match-id-e4fc4deb1ab06f92ce516934089a4acfb8d28195

--- lib/thirdparty/libhwsecurec/target.mk
+++
@@ -1,9 +0,0 @@
-#
-# Copyright 2017, Huawei Co. Ltd.
-#
-# See "HUAWEI_LICENSE" for details.
-#
-
-libs-$(CONFIG_LIB_HWSECUREC) += libhwsecurec libhwsecurec_a32
-libhwsecurec: common
-libhwsecurec_a32: common
