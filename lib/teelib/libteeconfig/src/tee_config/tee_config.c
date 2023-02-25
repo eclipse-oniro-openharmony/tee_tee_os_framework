9cf12689d0eac8c6cc9d4011c6c580703e3c86ba
xxx <xxx>
2023-02-25 18:40:24 +0800

    no message
    Match-id-2d41eea8a1f6782d77043c41adb9485c9e3c71a4

--- lib/teelib/libteeconfig/src/tee_config/tee_config.c
+++ lib/teelib/libteeconfig/src/tee_config/tee_config.c
@@ -33,26 +33,3 @@ struct drv_frame_info *get_drv_frame_infos(void)
 {
     return g_drv_frame_configs;
 }
-
-const struct ta_permission g_teeos_ta_permission_config[] = {
-    { TEE_SERVICE_REET, 0, SMC_GROUP_PERMISSION },
-#if (defined TEE_SUPPORT_HUK_SERVICE_32BIT || defined TEE_SUPPORT_HUK_SERVICE_64BIT)
-    { TEE_SERVICE_HUK, 0, GENERAL_GROUP_PERMISSION | CC_KEY_GROUP_PERMISSION | OEM_KEY_GROUP_PERMISSION},
-#endif
-#if (defined TEE_SUPPORT_PERM_64BIT || defined TEE_SUPPORT_PERM_32BIT)
-    { TEE_SERVICE_PERM, 0, PERMSRV_GROUP_PERMISSION | OEM_KEY_GROUP_PERMISSION },
-#endif
-};
-
-static const uint32_t g_teeos_ta_permission_num =
-    sizeof(g_teeos_ta_permission_config) / sizeof(g_teeos_ta_permission_config[0]);
-
-uint32_t get_teeos_ta_permission_num(void)
-{
-    return g_teeos_ta_permission_num;
-}
-
-const struct ta_permission *get_teeos_ta_permission_config(void)
-{
-    return g_teeos_ta_permission_config;
-}
